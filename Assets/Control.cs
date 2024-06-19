64c41f4c3ebc0e599c37ae8625b49cf3504790f8
manucoluccio <manucoluccio1105@gmail.com>
2024-06-19 10:38:33 -0300

    Ejercicios terminados.

---
+++ Assets/Control.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public GameObject[] comidas;
+    private int comidasmostradas = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            DeactivateAll();
+            comidas[comidasmostradas].SetActive(true);
+            if (comidasmostradas != comidas.Length - 1)
+            {
+                comidasmostradas++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            DeactivateAll();
+            comidas[comidasmostradas].SetActive(true);
+            if (comidasmostradas != 0)
+            {
+                comidasmostradas--;
+            }
+        }
+    }
+
+    void DeactivateAll()
+    {
+        foreach (var objeto in comidas)
+        {
+            objeto.SetActive(false);
+        }
+    }
+}
